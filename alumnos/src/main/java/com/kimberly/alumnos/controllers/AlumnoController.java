66cf66cba6491a718baae03f143900cb61c6b5b0
Anakimberly <22620053@tlaxiaco.tecnm.mx>
2025-05-12 00:30:24 -0600

    act5-creando metodo post en el controlador

--- alumnos/src/main/java/com/kimberly/alumnos/controllers/AlumnoController.java
+++ alumnos/src/main/java/com/kimberly/alumnos/controllers/AlumnoController.java
@@ -3,6 +3,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,11 +15,16 @@ import com.kimberly.alumnos.repository.AlumnoRepository;
 public class AlumnoController {
     @Autowired
     private AlumnoRepository alumnoRepository;
-
+//metodo get para traer todos los alumnos de la base de datos
     @GetMapping("/traer-alumnos")
     public List<Alumno> TraerAlumnos(){
         return alumnoRepository.findAll();
 
+    }
+    //metodo para insertar alumno en la base de datos
+    @PostMapping("/insertar-alumnos")
+    public Alumno insertarAlumno(@RequestBody Alumno alumno){
+        return alumnoRepository.save(alumno);
     }
 
 
