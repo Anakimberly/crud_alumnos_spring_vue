afe56d6a4b1fd1da8363b29e1bd57d514cbb57ab
Anakimberly <22620053@tlaxiaco.tecnm.mx>
2025-05-11 23:50:53 -0600

    se corrigio la clase alumno

--- alumnos/src/main/java/com/kimberly/alumnos/model/Alumno.java
+++ alumnos/src/main/java/com/kimberly/alumnos/model/Alumno.java
@@ -1,7 +1,13 @@
 package com.kimberly.alumnos.model;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 @Entity
 public class Alumno {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
      private String NumeroControl;
     private String nombre;
