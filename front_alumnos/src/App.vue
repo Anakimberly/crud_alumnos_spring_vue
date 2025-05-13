76072af99099fc73e30d9d29bca1ae945bdaf5cc
SandyMarissaGs <sandymarissa2004@gmail.com>
2025-05-13 02:29:49 -0600

    Act_13-Dando permisos a CrossOrigin

--- front_alumnos/src/App.vue
+++ front_alumnos/src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import {ref, onMounted} from 'vue';
+import { ref, onMounted } from 'vue';
 import axios from 'axios';
 
 const alumnos = ref([]);
@@ -15,12 +15,32 @@ onMounted(cargarAlumno);
 
 <template>
   <div class="container">
-    <button class="btn btn-primary">Boton</button>
+    <h2>Tabla de Alumnos</h2>
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">Id</th>
+          <th scope="col">Nombre</th>
+          <th scope="col">Apellidos</th>
+          <th scope="col">Carrera</th>
+          <th scope="col">Telefono</th>
+          <th scope="col">Imagen</th>
+          <th scope="col">Acciones</th>
+        </tr>
+      </thead>
+      <tr v-for="alumno in alumnos" :key="alumno.id">
+        <td>{{ alumno.id }}</td>
+        <td>{{ alumno.nombre }}</td>
+        <td>{{ alumno.apellidos }}</td>
+        <td>{{ alumno.carrera }}</td>
+        <td>{{ alumno.telefono }}</td>
+        <td><img :src="alumno.imagen" alt="Imagen de Alumno" width="50"></td>
+        <td><button class="btn btn-danger">Eliminar</button></td>
+      </tr>
+    </table>
   </div>
-  
+
 
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
