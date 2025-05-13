57796c5c59662769f3f03ed18fe06008d62120e9
SandyMarissaGs <sandymarissa2004@gmail.com>
2025-05-13 01:49:02 -0600

    Act_12-Creando metodo para traer la informaicon de los alumnos

--- front_alumnos/src/App.vue
+++ front_alumnos/src/App.vue
@@ -1,4 +1,16 @@
 <script setup>
+import {ref, onMounted} from 'vue';
+import axios from 'axios';
+
+const alumnos = ref([]);
+
+const cargarAlumno = async (params) => {
+  const response = await axios.get('localhost:8080/alumnos/traer-alumnos')
+  alumnos.value = response.data;
+  console.log(alumnos.value);
+}
+
+onMounted(cargarAlumno);
 </script>
 
 <template>
